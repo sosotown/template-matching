f63fc93cb9e12c3fe86692b740a4c50b7076cb0b
sosotown <bsk.sooo.1022@gmail.com>
2023-10-26 18:43:43 +0900

    test

--- hello.py
+++ hello.py
@@ -3,13 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 desktop_path = "/Users/sohatakeyama/Desktop/"
-img_rgb = cv2.imread(desktop_path + 'mario.png')
+img_rgb = cv2.imread(desktop_path + 'apex_sample.jpeg')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread(desktop_path + 'mario_coin.png', 0)
-w, h = template.shape[::-1)
+template = cv2.imread(desktop_path + 'predator.jpeg', 0)
+w, h = template.shape[::-1]
 
 res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
+threshold = 0.2
 loc = np.where(res >= threshold)
 
 # 重複を除外して異なる位置にマッチングした結果の数を数える
@@ -27,7 +27,6 @@ num_unique_matches = len(unique_locations)
 
 print("異なる位置にマッチングされた結果の数:", num_unique_matches)
 
-for pt in unique_locations:
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+for pt in unique_locations: cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
 cv2.imwrite(desktop_path + 'res.png', img_rgb)
